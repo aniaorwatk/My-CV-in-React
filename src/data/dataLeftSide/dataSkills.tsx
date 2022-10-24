03f91e91e851478beadc4fe7ef4351e1b2657332
nazwaaniaorwatk <orwat.ania@gmail.com>
2022-10-24 11:36:21 +0200

    add data left side

---
+++ src/data/dataLeftSide/dataSkills.tsx
@@ -0,0 +1,10 @@
+export default{
+    "title": "UMIEJĘTNOŚCI",
+   "data": [
+        {skill:"środowisko google" },
+        {skill:"pakiet ms office" },
+        {skill:"prawo jazdy kat. B" },
+        {skill:"j. angielski" },
+        {skill:"obsługa maszyn biurowych" },
+    ]
+}
