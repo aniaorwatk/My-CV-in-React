9a0cfd3e8179b9956eaf599d165fe92b25268810
nazwaaniaorwatk <orwat.ania@gmail.com>
2022-10-24 10:35:20 +0200

    create footer styles and comp

---
+++ src/components/CvPage/CvPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../Footer/Footer";
+import "./CvPage.scss"
+
+const CVPage =()=>{
+    return(
+           <div className='CvPage'>
+    <Footer/>
+  </div> 
+    )
+
+}
+
+export default CVPage
+
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import labels from "../../labels";
 import "./Footer.scss";
 
-const Footer =()=>{
-    return(
-        <footer>
-ulllla
-        </footer>
-    )
-}
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer__text">{labels.footer.textAgreement}</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
