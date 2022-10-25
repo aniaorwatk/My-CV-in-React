fa216bf53b3acdb989c1fe3c2bdc5f944f5c2ff6
nazwaaniaorwatk <orwat.ania@gmail.com>
2022-10-25 11:12:56 +0200

    add all data to right side

---
+++ src/data/dataRightSide/dataExperience.tsx
@@ -0,0 +1,66 @@
+export default {
+  title: "DOSWIADCZENIE ZAWODOWE",
+  data: [
+    {
+      job: "JUNIOR FRONT-END DEVELOPER TRAINEE",
+      company: "JCommerce Sp. z o.o. (staż)",
+      period_employment: "07.2021 - 08.2022",
+      job_responsibilities:
+        "• React • TypeScript • GitHub • GitLab • Redux Toolkit • React Router • React Testing Library • Responsive Web Design • Scrum • SASS • CSS • Git",
+    },
+    {
+      job: "SPECJALISTA DO SPRAW WINDYKACJI SADOWEJ",
+      company:
+        "TAURON Obsługa Klienta Sp. z o.o. za posrednictwem Work-Point I",
+      period_employment: "04.2021 - 01.2022",
+      job_responsibilities:
+        "• archiwizacja dokumentów oraz prowadzenie archiwum • kontakt z kancelariami komorniczymi • obsługa poczty • kompletowanie dokumentów do spraw windykacyjnych",
+    },
+    {
+      job: "PRACOWNIK ADMINISTRACYJNO-BIUROWY",
+      company: "APT-CERTA SP. Z O.O.",
+      period_employment: "2014 - 2021",
+      job_responsibilities:
+        "• wystawianie faktur • sporzadzanie dokumentów kadrowych • archiwizacja dokumentów • prowadzenie akt osobowych • wszystkie obowiazki z zakresu zatrudnienia i prowadzenia pracownika • kontakt z klientem i firmami outsourcingowymi • zarzadzanie przepływem informacji w firmie • prowadzenie korespondencji z urzedami i petentami • zamieszczanie ofert biznesowych na stronach internetowych • sporzadzanie sprawozdan, danych oraz wykresów",
+    },
+    {
+      job: "PRACOWNIK ADMINISTRACYJNO-BIUROWY",
+      company: "WORK4TIME Sp. z o. o. (um. zlecenie)",
+      period_employment: "2019 - 2020",
+      job_responsibilities:
+        "• sporzadzanie dokumentów kadrowych • wystawianie faktur",
+    },
+    {
+      job: "PRACOWNIK BIUROWY",
+      company: "A. Zatrudnienia nr certyfikatu: 6717",
+      period_employment: "2012 - 2014",
+      job_responsibilities:
+        "• sporzadzanie dokumentów kadrowych • wystawianie faktur • kontakt z klientem • zarządzanie przepływem informacji w firmie • prowadzenie korespondencji z urzędami i petentami",
+    },
+    {
+      job: "OBSŁUGA PUNKTU HANDLOWEGO",
+      company: "HANDEL-HURT, DETAL D. Kastelik",
+      period_employment: "2011 - 2012",
+      job_responsibilities: "• obsługa klienta, sprzedaz wyrobów cukierniczych",
+    },
+    {
+      job: "TELEMARKETER",
+      company: "Wydawnictwo B. Pietka",
+      period_employment: "2011",
+      job_responsibilities: "• sprzedaż telefoniczna",
+    },
+    {
+      job: "STAŻ, PRACOWNIK BIUROWY",
+      company: "US w Żywcu",
+      period_employment: "2009",
+      job_responsibilities:
+        "• sporzadzanie dokumentów dla petentów • prowadzenie korespondencji z innymi instytucjami • archiwizacja dokumentów",
+    },
+    {
+      job: "PRAKTYKA",
+      company: "US w Żywcu",
+      period_employment: "2008",
+      job_responsibilities: "• praca przy archiwizacji dokumentów",
+    },
+  ],
+};
---
+++ src/data/dataRightSide/dataProgrammingCourses.tsx
@@ -0,0 +1,50 @@
+export default {
+  title: "UKONCZONE KURSY PROGRAMOWANIA",
+  data: [
+    {
+      title_course: "Podstawy programowania",
+      name_platform: "LG Project",
+      certificate: "certyfikat: HTML, CSS, SCSS, JavaScript",
+    },
+    {
+        title_course: "Podstawy Web Developmentu",
+        name_platform: "Platforma Studiuje IT",
+        certificate: "certyfikat",
+      },
+      {
+        title_course: "Frontend Sredniozaawansowany",
+        name_platform: "Platforma Studiuje IT",
+        certificate: "certyfikat",
+      },
+      {
+        title_course: "Podstawy Programowania w JavaScript",
+        name_platform: "Platforma Studiuje IT",
+        certificate: "certyfikat",
+      },
+      {
+        title_course: "JavaScript",
+        name_platform: "Platforma udemy.pl",
+        certificate: "certyfikat",
+      },
+      {
+        title_course: "10 projektów w czystym JS",
+        name_platform: "Platforma udemy.p",
+        certificate: "certyfikat",
+      },
+      {
+        title_course: "Podstawy programowania",
+        name_platform: "LG Project",
+        certificate: "certyfikat",
+      },
+      {
+        title_course: "Learn React",
+        name_platform: "Platforma Scrimba",
+        certificate: "certyfikat",
+      },
+      {
+        title_course: "Advanced React",
+        name_platform: "Platforma Scrimba",
+        certificate: "certyfikat",
+      },
+  ],
+};
