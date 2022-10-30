2d4d74d6b14a2f8ee7032a5fa4d19eb716c35bda
ania orwatk <orwat.ania@gmail.com>
2022-10-30 19:28:12 +0100

    add comp edusation

--- src/components/RightSide/RightSideEducation/RightSideEducation.tsx
+++ src/components/RightSide/RightSideEducation/RightSideEducation.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import dataEducation from "../../../data/dataRightSide/dataEducation";
 import "./RightSideEducation.scss"
 
 const RightSideEducation =()=>{
     return(
         <div className="education">
-            
-
-
+             <h3 className="education__title">{dataEducation.title}</h3>
+             <p>{dataEducation.studies} - {dataEducation.period_studies}</p>
+             <p>{dataEducation.department}</p>
         </div>
     )
 }
